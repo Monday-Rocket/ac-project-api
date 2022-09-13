61be6ea33413b127c65ccd85314d2f08312dc622
Hee-jin506 <59636609+Hee-jin506@users.noreply.github.com>
2022-09-14 01:12:38 +0900

    feat: add gin

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,17 @@
 # syntax=docker/dockerfile:1
 
-FROM golang:1.16-alpine
+FROM golang:1.17-alpine
 
 WORKDIR /app
 
 COPY go.mod ./
+COPY go.sum ./
 RUN go mod download
 
 COPY *.go ./
 
 RUN go build -o /ac-project-api
 
-EXPOSE 5100
+EXPOSE 8080
 
 CMD [ "/ac-project-api" ]
--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
-import(
-	"log"
-	"net/http"
+import (
+  "net/http"
+
+  "github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("/hello", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Write([]byte("Hello"))
-	})
-
-	log.Fatal(http.ListenAndServe(":5100", nil))
+  r := gin.Default()
+  r.GET("/ping", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "pong",
+    })
+  })
+  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
